34374fe98adad63620924a3a8b57873cb4cdb5d9
felixoakz <feliz@oakz.org>
2022-10-04 11:41:15 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ cs50draft.py
@@ -0,0 +1,11 @@
+import csv
+teams = []
+# TODO: Read teams into memory from file
+
+with open('2018m.csv') as file:
+	reader = csv.DictReader(file)
+	for team in reader:
+		team['rating'] = int(team['rating'])
+		teams.append(team)
+
+print(team)
